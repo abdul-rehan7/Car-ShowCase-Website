81ef4b63a90a831acc7b5e6ceefe9bc49b695ad6
rehanmirza76 <abdulrehan0317@gmail.com>
2024-07-30 13:18:37 +0500

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      I am Hero
+    <div className='w-screen flex items-center justify-center  h-screen '>
+     <span className='font-extrabold text-9xl bg-gradient-to-b from-purple-500 to-blue-400 bg-clip-text text-transparent'> NEXT.JS 14</span>
     </div>
   )
 }
